16506d246bad8c37dc76c2873a9aeeeaf65c290e
Zoe <dadac123@gmail.com>
2014-05-26 18:54:57 +0800

    web.py

---
+++ code.py
@@ -0,0 +1,20 @@
+import web
+
+
+
+urls = (
+	'/','index'
+	)
+
+
+
+render = web.template.render('templates/')
+
+class index:
+	def GET(self):
+		name = 'Bob'
+		return render.index(name)
+
+if __name__ == '__main__':
+	app = web.application(urls, globals())
+	app.run()
